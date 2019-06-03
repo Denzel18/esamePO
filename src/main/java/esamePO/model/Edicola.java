c26a3023ff633d4344540380ddbaa8ab4285ace6
Denis Bernovschi <bernovschi.denis@gmail.com>
2019-06-03 14:17:36 +0200

    4 passo - Definizione di altri metodi di base della classe Edicola.java

--- src/main/java/esamePO/model/Edicola.java
+++ src/main/java/esamePO/model/Edicola.java
@@ -1,5 +1,7 @@
 package esamePO.model;
 
+import java.util.Arrays;
+
 public class Edicola {
 	private String Codice;
 	private String Ubicazione;
@@ -17,8 +19,9 @@ public class Edicola {
 	
 	
 	
+
 	public Edicola(String codice, String ubicazione, String areaDiCompetenza, String descrizioneVia, int civico,
-			int codiceVia, String localita, String forma_vendita_edicole, boolean vendita_esclusiva, int Municipio,
+			int codiceVia, String localita, String formaVenditaEdicole, boolean venditaEsclusiva, int municipio,
 			float lONGIT, float lATIT, float[] location) {
 		super();
 		this.Codice = codice;
@@ -28,9 +31,9 @@ public class Edicola {
 		this.Civico = civico;
 		this.CodiceVia = codiceVia;
 		this.Localita = localita;
-		this.FormaVenditaEdicole = forma_vendita_edicole;
-		this.VenditaEsclusiva = vendita_esclusiva;
-		this.Municipio = Municipio;
+		this.FormaVenditaEdicole = formaVenditaEdicole;
+		this.VenditaEsclusiva = venditaEsclusiva;
+		this.Municipio = municipio;
 		this.LONGIT = lONGIT;
 		this.LATIT = lATIT;
 		this.Location = location;
@@ -114,8 +117,91 @@ public class Edicola {
 	protected void setLocation(float[] location) {
 		Location = location;
 	}
-	
-	
-	
 
+	@Override
+	public String toString() {
+		return "Edicola [Codice=" + Codice + ", Ubicazione=" + Ubicazione + ", AreaDiCompetenza=" + AreaDiCompetenza
+				+ ", DescrizioneVia=" + DescrizioneVia + ", Civico=" + Civico + ", CodiceVia=" + CodiceVia
+				+ ", Localita=" + Localita + ", FormaVenditaEdicole=" + FormaVenditaEdicole + ", VenditaEsclusiva="
+				+ VenditaEsclusiva + ", Municipio=" + Municipio + ", LONGIT=" + LONGIT + ", LATIT=" + LATIT
+				+ ", Location=" + Arrays.toString(Location) + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((AreaDiCompetenza == null) ? 0 : AreaDiCompetenza.hashCode());
+		result = prime * result + Civico;
+		result = prime * result + ((Codice == null) ? 0 : Codice.hashCode());
+		result = prime * result + CodiceVia;
+		result = prime * result + ((DescrizioneVia == null) ? 0 : DescrizioneVia.hashCode());
+		result = prime * result + ((FormaVenditaEdicole == null) ? 0 : FormaVenditaEdicole.hashCode());
+		result = prime * result + Float.floatToIntBits(LATIT);
+		result = prime * result + Float.floatToIntBits(LONGIT);
+		result = prime * result + ((Localita == null) ? 0 : Localita.hashCode());
+		result = prime * result + Arrays.hashCode(Location);
+		result = prime * result + Municipio;
+		result = prime * result + ((Ubicazione == null) ? 0 : Ubicazione.hashCode());
+		result = prime * result + (VenditaEsclusiva ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Edicola other = (Edicola) obj;
+		if (AreaDiCompetenza == null) {
+			if (other.AreaDiCompetenza != null)
+				return false;
+		} else if (!AreaDiCompetenza.equals(other.AreaDiCompetenza))
+			return false;
+		if (Civico != other.Civico)
+			return false;
+		if (Codice == null) {
+			if (other.Codice != null)
+				return false;
+		} else if (!Codice.equals(other.Codice))
+			return false;
+		if (CodiceVia != other.CodiceVia)
+			return false;
+		if (DescrizioneVia == null) {
+			if (other.DescrizioneVia != null)
+				return false;
+		} else if (!DescrizioneVia.equals(other.DescrizioneVia))
+			return false;
+		if (FormaVenditaEdicole == null) {
+			if (other.FormaVenditaEdicole != null)
+				return false;
+		} else if (!FormaVenditaEdicole.equals(other.FormaVenditaEdicole))
+			return false;
+		if (Float.floatToIntBits(LATIT) != Float.floatToIntBits(other.LATIT))
+			return false;
+		if (Float.floatToIntBits(LONGIT) != Float.floatToIntBits(other.LONGIT))
+			return false;
+		if (Localita == null) {
+			if (other.Localita != null)
+				return false;
+		} else if (!Localita.equals(other.Localita))
+			return false;
+		if (!Arrays.equals(Location, other.Location))
+			return false;
+		if (Municipio != other.Municipio)
+			return false;
+		if (Ubicazione == null) {
+			if (other.Ubicazione != null)
+				return false;
+		} else if (!Ubicazione.equals(other.Ubicazione))
+			return false;
+		if (VenditaEsclusiva != other.VenditaEsclusiva)
+			return false;
+		return true;
+	}
+	
+	
 }
