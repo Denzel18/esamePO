d9164893d3924f35381f5d05a7466d44f3c6f719
Denis Bernovschi <bernovschi.denis@gmail.com>
2019-06-08 09:18:30 +0200

    8.4 passo - controllo parser json, check sul file .csv e download di quest'ultimo

---
+++ src/main/java/esamePO/springboot/service/CSVParser.java
@@ -0,0 +1,17 @@
+package esamePO.springboot.service;
+
+public class CSVParser {
+	
+	private String nomeFile ; 
+	
+	public CSVParser(String nome) {
+		this.nomeFile = nome; 
+	}
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
